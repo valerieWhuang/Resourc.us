24d7ce91aec74cefc718001fdd2b7bf5172ea291
jenpoole <contact.vandepool@gmail.com>
2021-04-14 22:18:18 -0400

    #44 add Multiselect Dropdown for tags

--- src/components/CreateResource.jsx
+++ src/components/CreateResource.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import { Multiselect } from 'multiselect-react-dropdown';
 
 function createResource() {
   const currentTeam = localStorage.getItem('currentTeam')
@@ -16,7 +17,8 @@ function createResource() {
     votes: 0,
     tags: [],
     commentsList: [],
-    postedBy: currentUser // user is auto-populated if logged in
+    postedBy: currentUser, // user is auto-populated if logged in
+    relatedLocation: ""
   });
 
   const [_teams, setTeams] = useState([]);
@@ -33,11 +35,52 @@ function createResource() {
         console.log("GET FAILED", err);
       });
   }, []);
+
+  // tag options
+  const [ tagOptions, setTagOptions ] = useState([])
+  const [ newTagOption, setNewTagOption] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:3000/tags/list")
+      .then(response => response.json())
+      .then( data => {
+        console.log('tag options', data)
+        setTagOptions(data)
+      })
+      .catch( err => {
+        console.log("FAILED TO GET TAG OPTIONS", err)
+      })
+  }, [])
+
+  // during tag search, capture search query 
+  function handleTagSearch() {
+    const newValues = document.getElementById('search_input').value
+    setNewTagOption(newValues)
+    console.log('newValues: ', newValues)
+  }
+
+  // if tag doesn't exist in DB,
+  // create it and update the tag options in state
+  function createNewTag(e) {
+    e.preventDefault()
+    console.log('newTagOption:', newTagOption)
+  }
+
+  function handleTagSelect(selectedList, selectedItem) {
+    console.log(selectedList, 'selectedItem:', selectedItem)
+  }
+
+  function handleTagRemove(selectedList, removedItem) {
+    console.log(selectedList, 'removedItem:', removedItem)
+  }
+
+
   function handleChange(event) {
     const { name, value } = event.target; //event target is each indivisual form that is being inputed
     console.log(_payload);
     setPayload({ ..._payload, [name]: value }); // copies previous state and updates only changed key/values
   }
+
   function handleClick(event) {
     event.preventDefault();
     //test if server is working
@@ -104,29 +147,19 @@ function createResource() {
             placeholder="Description"
           ></input>
         </div>
-        <div className="form-group">
-          <input
-            onChange={handleChange}
-            name="tags"
-            autoComplete="off"
-            value={_payload.tags}
-            className="form-control"
-            placeholder="Tags"
-          ></input>
-        </div>
 
-        {/* team is preselected if already on team page */}
-        {/* <div className="form-group">
-          <input
-            type="hidden"
-            onChange={handleChange}
-            name="team"
-            autoComplete="off"
-            value=""
-            className="form-control"
-            placeholder=""
-          ></input>
-        </div> */}
+        {/* Tag Multiselect Component */}
+        <Multiselect
+          options={tagOptions} // Options to display in the dropdown
+          selectedValues="" // Preselected value to persist in dropdown
+          displayValue="name" // Property name to display in the dropdown options
+          onSearch={handleTagSearch}
+          onSelect={handleTagSelect} // Function will trigger on select event
+          onRemove={handleTagRemove} // Function will trigger on remove event
+          emptyRecordMsg={<p>{newTagOption} <button onClick={createNewTag} className="btn btn-sm btn-primary">Create tag</button></p>}
+          closeIcon="circle"
+        />
+  
 
         {/* <select onChange={selectTeam}>
           {_teams.map((team) => (
@@ -134,7 +167,7 @@ function createResource() {
           ))}
         </select> */}
 
-        <button onClick={handleClick} className="btn btn-lg btn-info">
+        <button onClick={handleClick} className="btn btn-lg btn-info" style={{marginTop: "100px"}}>
           Create Resource
         </button>
       </form>
